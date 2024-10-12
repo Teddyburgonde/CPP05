0d256392f2e04cad68987ea613f47255cc9737a5
Teddyburgonde <bandama.teddy@gmail.com>
2024-10-12 18:38:11 +0200

    ex00 finish

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,85 +1,17 @@
 #include "Bureaucrat.hpp"
-#include <exception>
-
-// int main()
-// {
-// 	try
-// 	{
-// 		throw Bureaucrat::GradeTooHighException();
-// 	}
-// 	catch (const std::exception &e)
-// 	{
-// 		std::cout << e.what() << std::endl; 
-// 	}
-//   	return 0;
-// }
 
 int	main()
 {
-	//Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name), _grade(grade)
-
 	try 
 	{
-		Bureaucrat b("Pierre", 1);
+		Bureaucrat b("Galaad", 3);
+		b.incrementGrade();
+		b.incrementGrade();
 		b.incrementGrade();
 		std::cout << b << std::endl;
-
-	} 
-	catch (const std::exception &e) 
+	
+	} catch (std::exception &e) 
 	{
 		std::cout << e.what() << std::endl;
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// try
-	// {
-	// 	Bureaucrat b("Bureaucrat", 150);
-
-	// 	std::cout << b << std::endl;
-	// 	b.incrementGrade();
-	// 	std::cout << b << std::endl;
-	// 	b.incrementGrade();
-	// 	std::cout << b << std::endl;
-	// 	b.incrementGrade();
-	// 	std::cout << b << std::endl;
-	// 	b.incrementGrade();
-	// 	std::cout << b << std::endl;
-	// 	b.incrementGrade();
-	// 	std::cout << b << std::endl;
-	// }
-	// catch (const std::exception &e)
-	// {
-	// 	std::cout << e.what() << std::endl;
-	// }
-	// return 0;
 }
