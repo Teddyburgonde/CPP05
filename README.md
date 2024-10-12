edc726c1307ca77866c22cfcf83ad0c1d45b6feb
Teddyburgonde <bandama.teddy@gmail.com>
2024-10-12 18:00:31 +0200

    finish, to do verif

--- README.md
+++ README.md
@@ -2,23 +2,26 @@
 
 - Les classes d'exceptions n'ont pas besoin de la forme Canonique.
 
-.hpp
-class Bureaucrat
-private :
-	std::string const _name;
-	int grade;
-- grade valid ? de 1 a 150.
 **Les exceptions**
-
 - Une exception en programmation est un mécanisme qui permet de gérer les erreurs ou les situations inattendues de manière propre et contrôlée.
-- Pour les grades utiliser les exceptions. 
-- On doit intercepter les exceptions qu'on leve dans un bloc de try-catch
-- La signiatures des exceptions ce met dans le main. on rajoute : public std::exception
-- What ???
-- Bureaucrat::GradeTooHighException
-- Bureaucrat::GradeTooLowException.
+
+```c
+include <exception>
+
+int	main()
+{
+	try
+	{
+        // code qui pourrait ou pas generer une exception 
+	}
+	catch (const std::exception &e)
+	{
+		// capture et gestion de l'exception 
+	}
+}
+```
+
 - virtual const char* what() const throw() cela veut dire que la fonction what return une chaine de caractere constant.
-- include <exception>
 - class GradeTooHighException : public std::exception
 {
 public:
@@ -27,20 +30,6 @@ public:
         return "Grade is too high!";
     }
 };
-- noexcept signifie que la fonction ne levera pas d'exception supplémentaire.
-
-
-
-
-.cpp
-- return "message"
-- What ???
-
-
-
-
-
-
 
 ❌
 ✅
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,16 +1,85 @@
 #include "Bureaucrat.hpp"
+#include <exception>
 
+// int main()
+// {
+// 	try
+// 	{
+// 		throw Bureaucrat::GradeTooHighException();
+// 	}
+// 	catch (const std::exception &e)
+// 	{
+// 		std::cout << e.what() << std::endl; 
+// 	}
+//   	return 0;
+// }
 
-
-int main()
+int	main()
 {
-	try
+	//Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name), _grade(grade)
+
+	try 
 	{
-		throw Bureaucrat::GradeTooHighException();
-	}
-	catch (const std::exception &e)
+		Bureaucrat b("Pierre", 1);
+		b.incrementGrade();
+		std::cout << b << std::endl;
+
+	} 
+	catch (const std::exception &e) 
 	{
-		std::cout << e.what() << std::endl; 
+		std::cout << e.what() << std::endl;
 	}
-  	return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// try
+	// {
+	// 	Bureaucrat b("Bureaucrat", 150);
+
+	// 	std::cout << b << std::endl;
+	// 	b.incrementGrade();
+	// 	std::cout << b << std::endl;
+	// 	b.incrementGrade();
+	// 	std::cout << b << std::endl;
+	// 	b.incrementGrade();
+	// 	std::cout << b << std::endl;
+	// 	b.incrementGrade();
+	// 	std::cout << b << std::endl;
+	// 	b.incrementGrade();
+	// 	std::cout << b << std::endl;
+	// }
+	// catch (const std::exception &e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
+	// return 0;
 }
